96bec55d940eafa5afea743819c22f959089db81
iancuioan <iancuioan897@yahoo.ro>
2026-08-15 20:17:54 +0300

    set debug False

--- bzzz_core/settings.py
+++ bzzz_core/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = os.getenv('DEBUG')
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = ["bzzz-home.onrender.com"]
 
