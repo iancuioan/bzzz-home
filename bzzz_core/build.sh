755bda6e5c181a486caf84e27f2abd3e8c4fc98b
iancuioan <iancuioan897@yahoo.ro>
2026-08-22 22:46:57 +0300

    build

---
+++ bzzz_core/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Installing project dependencies..."
+python3 -m pip install -r requirements.txt
+
+echo "Making migrations..."
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+
+echo "Collecting static files..."
+python3 manage.py collectstatic --noinput
